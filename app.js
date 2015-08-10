6c1111f4b1df4f0c04db5d72f01f10877bf1b19e
bmacheski <bwm0007@gmail.com>
2015-08-10 17:04:47 -0500

    add marker functionality for connected users

--- app.js
+++ app.js
@@ -10,7 +10,25 @@ app.get('/', function (req, res) {
   res.render('index', {});
 })
 
-io.on('connection', function () {})
+var users = [
+    {id: 1234, latitude: 33.7550, longitude: -84.3900},
+    {id: 4567, latitude: 32.7833, longitude: -79.9333}
+];
+
+io.on('connection', function (socket) {
+  // upon initial connection adds location info/id to users array
+  socket.on('connected user', function (data, id) {
+    users.push({
+      id: id,
+      latitude: data.G,
+      longitude: data.K
+    })
+  })
+  // sends users location data to create markers
+  socket.on('get locations', function (dataFunc) {
+    dataFunc(users)
+  })
+})
 
 http.listen(3000, function () {
   console.log('Listening at http://localhost:3000')
--- public/js/map.js
+++ public/js/map.js
@@ -1,5 +1,7 @@
 function initialize () {
+  var userId = Math.random().toString(15).substring(2, 14);
   var socket = io();
+  var markers = {};
   var mapOptions = {
     zoom: 6,
     center: new google.maps.LatLng(36.174465, -86.767960)
@@ -10,7 +12,32 @@ function initialize () {
   function createMap (pos) {
     var lat = pos.coords.latitude;
     var lng = pos.coords.longitude;
-    var myLatLlng = new google.maps.LatLng(lat, lng);
+    var myLatlng = new google.maps.LatLng(lat, lng);
+    socket.emit('connected user', myLatlng, userId);
+    createMarker(myLatlng, userId)
+  }
+  // get locations of already connected users
+  socket.emit('get locations', createConnectedMarkers);
+
+  function createMarker (markloc, uuid) {
+    marker = new google.maps.Marker({
+      id: uuid,
+      position: markloc,
+      map: map
+    })
+    markers[uuid] = marker;
+  }
+
+  // loops through collection of connected users data
+  function createConnectedMarkers (data) {
+    var connected = data;
+    for(var i=0; i<connected.length; i++){
+      var uid = connected[i].id;
+      var lat = connected[i].latitude;
+      var lng = connected[i].longitude;
+      var loc = new google.maps.LatLng(lat, lng);
+      createMarker(loc, uid);
+    }
   }
 }
 
